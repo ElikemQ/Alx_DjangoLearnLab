c47387b93b19eaaab059dc4a844c7922d4b329b0
ElikemQ <elikemgbotsyo@gmail.com>
2025-02-22 22:37:40 +0000

    added login.html, logout.html, register.html and their views and urls to repo

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
 from .import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
-    path('register/', views.register, name='register'),
     
+
+    path("register/", views.register_view, name="register"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("", auth_views.TemplateView.as_view(template_name="registration_app/home.html"), name="home")
     
     
     
