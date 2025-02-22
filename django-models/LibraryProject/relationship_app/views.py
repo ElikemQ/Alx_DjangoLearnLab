aee7aa6b6611fc0a0d5ef74915722557b16d5b37
ElikemQ <elikemgbotsyo@gmail.com>
2025-02-22 22:29:44 +0000

    added login.html, logout.html, register.html and their views and urls to repo

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -20,3 +20,35 @@ class LibraryDetailView(DetailView):
     def get_queryset(self):
         library_id = self.kwargs['library_id']
         return Book.objects.filter(library__id=library_id)
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()  # Save new user
+            username = form.cleaned_data.get('username')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form':form}) 
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)  
+            return redirect('home') 
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)  
+    return redirect('login')
