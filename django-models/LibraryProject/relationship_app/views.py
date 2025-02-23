c96217edb92d3cab9f08931e2da9ace201f9de92
ElikemQ <elikemgbotsyo@gmail.com>
2025-02-23 20:39:49 +0000

    modified urls,models and views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -92,24 +92,24 @@ def member_view(request):
     return render(request, 'member_view.html')
 
 
-# def is_admin(user):
-#     return check_role(user, 'Admin')
+def is_admin(user):
+    return check_role(user, 'Admin')
 
-# def is_librarian(user):
-#     return check_role(user, 'Librarian')
+def is_librarian(user):
+    return check_role(user, 'Librarian')
 
-# def is_member(user):
-#     return check_role(user, 'Member')
+def is_member(user):
+    return check_role(user, 'Member')
 
-# @user_passes_test(is_admin)
-# def admin_view(request):
-#     return render(request, 'relationship_app/admin_view.html')
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
 
 
-# @user_passes_test(is_librarian)
-# def librarian_view(request):
-#     return render(request, 'relationship_app/librarian_view.html')
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
 
-# @user_passes_test(is_member)
-# def member_view(request):
-#     return render(request, 'relationship_app/member_view.html')
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
