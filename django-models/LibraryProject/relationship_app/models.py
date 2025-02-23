c089e858e0589f66f4552503ae47ebc24247cc1f
ElikemQ <elikemgbotsyo@gmail.com>
2025-02-23 23:33:12 +0000

    updated adminviews

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-# from django.db.models import post_save 
+from django.db.models import post_save 
 # Create your models here.
 
 class Author(models.Model):
@@ -57,13 +57,13 @@ class UserProfile(models.Model):
         return f"{self.user.username} - {self.role}"
 
 
-# @receiver(post_save, sender=User)
-# def create_user_profile(sender, instance, created, **kwargs):
-#     if created:
-#         UserProfile.objects.create(user=instance)
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
 
-# @receiver(post_save, sender=User)
-# def save_user_profile(sender, instance, **kwargs):
-#     instance.userprofile.save()
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
 
 
