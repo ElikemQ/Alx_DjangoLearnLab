300fc2028c330a63159ae067b2c8c8eff2cb236e
ElikemQ <elikemgbotsyo@gmail.com>
2025-02-23 01:33:31 +0000

    modified access control files for admin, user and librarian

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -12,9 +12,9 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', 'relationship_app.views.register', name='register'),
     
-    path('admin/', views.admin_view, name='admin_view'),
-    path('librarian/', views.librarian_view, name='librarian_view'),
-    path('member/', views.member_view, name='member_view'),
+    path('admin/', admin_view, name='admin_view'),
+    path('librarian/',librarian_view, name='librarian_view'),
+    path('member/', member_view, name='member_view'),
     
     ]
 
