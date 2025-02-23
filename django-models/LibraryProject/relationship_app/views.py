edfefae6d2564793b2fed8bffcba0d73285858cb
ElikemQ <elikemgbotsyo@gmail.com>
2025-02-23 20:44:51 +0000

    modified urls,models and views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -93,13 +93,13 @@ def member_view(request):
 
 
 def is_admin(user):
-    return check_role(user, 'Admin')
+    return user.userprofile.role == 'Admin'
 
 def is_librarian(user):
-    return check_role(user, 'Librarian')
+    return user.userprofile.role == 'Librarian'
 
 def is_member(user):
-    return check_role(user, 'Member')
+    return user.userprofile.role == 'Member'
 
 @user_passes_test(is_admin)
 def admin_view(request):
