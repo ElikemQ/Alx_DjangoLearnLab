718525d85c2e21d3193770fd97b2125c26785a93
ElikemQ <elikemgbotsyo@gmail.com>
2025-03-09 23:14:48 +0000

    implemented authentication and permissions in django rest framework

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,9 @@
-
 from django.urls import path
 from .views import BookList
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'), 
+    path('books/', BookList.as_view(), name='book-list'),
+    path('api-token-auth/', obtain_auth_token, name = 'api_token_auth',) 
+
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,10 +3,65 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework import generics
 from .models import Book
-from .serializers import BookSerializer
+from .serializers import BookSerializer, MyModelSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import viewsets
+from .models import MyModel
+
 
 # api view 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+
+
+
+
+
+#authentications 
+class MyProtectedView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({"message": "This is view is protected."})
+    
+class AdminOnlyView(APIView):
+    permission_classes = [IsAdminUser]
+
+    def get(self, request):
+        return Response({"message": "This is view is for admin-only."})
+    
+
+class PublicView(APIView):
+    permission_classes = [AllowAny] 
+
+    def get(self, request):
+        return Response({"message": "This view is open to everyone."})
+    
+
+# viewset permissions
+class MyModelViewSet(viewsets.ModelViewSet):
+    queryset = MyModel.objects.all()
+    serializer_class = MyModelSerializer
+
+    permission_classes = [IsAuthenticated]  
+
+    def get_permissions(self):
+        """
+        Override to add different permissions for different actions.
+        """
+        if self.action == 'create':
+            return [IsAdminUser()]  
+        elif self.action in ['update', 'partial_update', 'destroy']:
+            return [IsAdminUser()]  
+        return super().get_permissions()
+
+
+
+
+    
 
