b3fc879bd5a6f34a42f3fec5a27dfaed977e34a7
ElikemQ <elikemgbotsyo@gmail.com>
2025-03-30 23:53:25 +0000

    implementing notifications and likes functionality

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -12,8 +12,8 @@ router.register(r'comments', CommentViewSet, basename='comment')
 urlpatterns = [
     path('', include(router.urls)),
     path('feed/', FeedView.as_view(), name='user_feed'), 
-    path('posts/<int:pk>/like', views.like_post, name='like_post'),
-    path('posts/<int:pk>/unlike', views.unlike_post, name='unlike_post'),
+    path('<int:pk>/like/', views.like_post, name='like_post'),
+    path('<int:pk>/unlike/', views.unlike_post, name='unlike_post'),
 
 
 
